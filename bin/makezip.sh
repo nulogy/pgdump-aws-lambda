93025cd29f1e7652bd042593b9f48af7a2d2eef1
Steph <stephaniem@nulogy.com>
2018-07-03 16:17:32 -0400

    Modifies script to work in mac

--- bin/makezip.sh
+++ bin/makezip.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-SCRIPT=`readlink -f $0`
-SCRIPTPATH=`dirname $SCRIPT`
-PROJECTROOT=`readlink -f $SCRIPTPATH/..`
 FILENAME="pgdump-aws-lambda.zip"
 
 command_exists () {
@@ -15,40 +12,34 @@ if ! command_exists zip ; then
     exit 0
 fi
 
-
-cd $PROJECTROOT
-
 echo "creating bundle.."
 # create a temp directory for our bundle
-BUNDLE_DIR=$(mktemp -d)
+mkdir bundle
 # copy entire app into BUNDLE_DIR
-cp -r * $BUNDLE_DIR/
+cp -r `ls -A | grep -v "bundle"` bundle
 
 # prune things from BUNDLE_DIR
 echo "running npm prune.."
-cd $BUNDLE_DIR
+cd bundle
 # prune dev-dependancies from node_modules
 npm prune --production >> /dev/null
 
 rm -rf dist coverage test
 
-
 # create and empty the dist directory
-if [ ! -d $PROJECTROOT/dist ]; then
-    mkdir $PROJECTROOT/dist
+if [ ! -d ../dist ]; then
+    mkdir ../dist
 fi
-rm -rf $PROJECTROOT/dist/*
+rm -rf ../dist/*
 
 # create zip of bundle/
 echo "creating zip.."
 zip -q -r $FILENAME *
 echo "zip -q -r $FILENAME *"
-mv $FILENAME $PROJECTROOT/dist/$FILENAME
+mv $FILENAME ../dist/$FILENAME
 
 echo "successfully created dist/$FILENAME"
 
+cd ..
 # remove bundle/
-rm -rf $BUNDLE_DIR
-
-
-cd $PROJECTROOT
+rm -rf bundle/
